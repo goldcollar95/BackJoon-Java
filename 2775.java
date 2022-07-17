e57d33ad19980e2942797c14cfa0c7f4e81c078f
Sangmun Kim <98082293+goldcollar95@users.noreply.github.com>
2022-07-17 20:13:56 +0900

    implement

---
+++ 2775.java
@@ -0,0 +1,46 @@
+//https://www.acmicpc.net/problem/2775
+
+import java.util.Scanner;
+
+public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner fr = new Scanner(System.in);
+		
+		int[][] APT = new int[15][15];
+		
+		for(int i = 0; i < 15; i++) {
+			APT[i][1] = 1;
+			APT[0][i] = i;	
+		}
+		
+		for(int i = 1; i < 15; i++) {
+			for(int j = 2; j < 15; j++) {
+				APT[i][j] = APT[i][j-1] + APT[i-1][j];	//**
+				
+			
+			}
+		}
+		
+		int T = fr.nextInt(); 
+		
+		for(int i=0; i<T; i++) {
+			int k = fr.nextInt();
+			int n = fr.nextInt();
+			System.out.println(APT[k][n]);
+		}
+		
+			
+		}	//main end
+	} //class end
+
+		
+		
+
+		
+		
+	
+
+
